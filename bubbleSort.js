c1c08b9253a9fe1c7b395ede9776bc5de0c520fc
Harsh Singh <168174225+harry10352@users.noreply.github.com>
2024-12-16 13:06:12 +0530

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+console.log("After sorting:", bubbleSort([5, 3, 8, 4, 2])); // [2, 3, 4, 5, 8];
